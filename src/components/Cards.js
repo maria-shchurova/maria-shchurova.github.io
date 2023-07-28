2e28a5d110a3792c42c3934598af941bb7c1f600
Maria Shchurova <mary.shchurova@gmail.com>
2023-07-28 19:32:30 +0200

    try another update (deploy from Actions)

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,6 +6,7 @@ import "./Card.css"
 import image1 from "../../src/assets/images/GameCovers/AfterAll.png"
 import image2 from "../../src/assets/images/GameCovers/BreathOfLife.png"
 import image3 from "../../src/assets/images/GameCovers/ForRent.jpg"
+import image4 from "../../src/assets/images/GameCovers/Elkridge.png"
 
 function Cards() {
   return (
@@ -40,19 +41,19 @@ function Cards() {
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
                     <CardItem 
-                    src={image1}
+                    src={image4}
                     title="After All"
                     info="A point and click adventure game about going through a breakup."
                     path="https://firewalkwithme.itch.io/after-all"
                     />
                     <CardItem 
-                    src={image2}
+                    src={image4}
                     title="Breath Of Life"
                     info="Simulation experiment. Prodedurally growng plants overtake abandoned buildings."
                     path={"https://firewalkwithme.itch.io/breath-of-life"}
                     />
                     <CardItem 
-                    src={image3}
+                    src={image4}
                     title="For Rent"
                     info="First-person horror game about a rented soviet flat."
                     path="https://firewalkwithme.itch.io/for-rent"
